b80e286b071ff58e46639f5a84f3ce0176c5aa7e
Tamizharasu18 <tamizharasu@dezvolta.com>
2024-11-14 15:55:01 +0530

    animation

--- src/Component/GoodInsights/GoodInsights.jsx
+++ src/Component/GoodInsights/GoodInsights.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const GoodInsights = () => {
     return (
-        <div className='h-[100vh] flex justify-center items-center'>
-            <div className='text-center max-w-[600px]'>
+        <div className='h-[100vh] flex justify-center items-center px-[20px] md:px-[60px]'>
+            <div className='text-center md:max-w-[600px]'>
                 <h2 className='text-[34px] font-bold leading-[42px] text-[#220d4e] mb-[20px]'>It’s easy to make good choices when you have good insights</h2>
                 <p className='text-[18px] leading-[30.34px] text-[#514b5f]'>Our open platform provides the measurement, analytics, engagement, and fraud protection technologies you need to get the answers to all of your questions, and make good choices for your business and customers.</p>
             </div>
