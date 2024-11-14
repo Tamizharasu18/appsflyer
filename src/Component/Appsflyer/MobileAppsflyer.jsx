d3155780f3bad4f0d908df7b60719a4ee77e5ec7
Tamizharasu18 <tamizharasu@dezvolta.com>
2024-11-14 16:32:54 +0530

    animation

---
+++ src/Component/Appsflyer/MobileAppsflyer.jsx
@@ -0,0 +1,186 @@
+import React from 'react'
+import Image from 'next/image'
+
+const MobileAppsflyer = () => {
+    return (
+        <div>
+            <div className='w-[100%]'>
+                <div className='w-[100%] relative top-[100px]'>
+                    <div className='flex justify-center'>
+                        <Image
+                            src={'/New folder/USP_1-1.svg'}
+                            width={800}
+                            height={800}
+                            alt=''
+                            className='' />
+                    </div>
+                    <div className='pb-[40px] pt-[30px]'>
+                        <h2 className='text-[28px] font-bold leading-[34px] text-center mb-[5px] text-[#220d4e]'>Can you accurately measure your marketing while protecting customer privacy?</h2>
+                        <p className='text-[18px] leading-[30.34px] mb-[20px] text-center font-normal text-[#514b5f] pt-[30px]'>Bringing in new customers is great. Getting accurate insights while protecting your customers’ privacy is even better. Our privacy-preserving measurement and cost aggregation technologies give you insights you can count on across channels, platforms, and devices.</p>
+                        <div className='flex items-center justify-center gap-x-[5px] hover:gap-x-[10px] cursor-pointer py-[10px] transition-all duration-300'>
+                            <div className='arrow '>
+                                <span className='text-[16px] font-medium leading-[21px] pb-[10px] btn-arrow text-[#220d4e]'>Explore our measurement suite</span>
+                            </div>
+                            <div>
+                                <Image
+                                    src={"/right-arrow.svg"}
+                                    width={20}
+                                    height={20}
+                                    alt=''
+                                    className='w-[15px] h-[15px]'
+                                />
+                            </div>
+                        </div>
+                        <div className='flex items-center justify-center  gap-x-[5px] hover:gap-x-[10px] cursor-pointer py-[10px] transition-all duration-300'>
+                            <div className='arrow'>
+                                <span className='text-[16px] font-medium leading-[21px] pb-[10px] btn-arrow text-[#220d4e]'>Explore our SKAdNetwork solution</span>
+                            </div>
+                            <div>
+                                <Image
+                                    src={"/right-arrow.svg"}
+                                    width={20}
+                                    height={20}
+                                    alt=''
+                                    className='w-[15px] h-[15px]'
+                                />
+                            </div>
+                        </div>
+                        <div className='flex items-center justify-center gap-x-[5px] hover:gap-x-[10px] cursor-pointer py-[10px] transition-all duration-300'>
+                            <div className='arrow'>
+                                <span className='text-[16px] font-medium leading-[21px] pb-[10px] btn-arrow text-[#220d4e]'>Explore our ROI measurement solution</span>
+                            </div>
+                            <div>
+                                <Image
+                                    src={"/right-arrow.svg"}
+                                    width={20}
+                                    height={20}
+                                    alt=''
+                                    className='w-[16px] h-[16px] '
+                                />
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+                    <div className='py-[40px]'>
+                        <div className='flex justify-center'>
+                            <Image
+                                src={'/New folder/USP_2-1.svg'}
+                                width={800}
+                                height={800}
+                                alt=''
+                                className='' />
+                        </div>
+                        <h2 className='text-[28px] font-bold leading-[34px] text-center mb-[5px] pt-[30px] text-[#220d4e]'>Can you build a winning creative strategy that is based on performance data and AI insights?</h2>
+                        <p className='text-[18px] leading-[30.34px] mb-[20px] text-center font-normal text-[#514b5f] pt-[30px]'>When it comes to building a winning creative formula, leveraging performance data and AI is crucial. With our AI-powered solution, you can harness the power of data and advanced technology to create a creative strategy that is supported by reliable performance metrics and valuable AI-driven insights.</p>
+                        <div className='flex items-center justify-center gap-x-[5px] hover:gap-x-[10px] cursor-pointer py-[10px] transition-all duration-300'>
+                            <div className='arrow'>
+                                <span className='text-[16px] font-medium leading-[21px] pb-[10px] btn-arrow text-[#220d4e]'>Explore our Creative Optimization solution</span>
+                            </div>
+                            <div>
+                                <Image
+                                    src={"/right-arrow.svg"}
+                                    width={20}
+                                    height={20}
+                                    alt=''
+                                    className='w-[15px] h-[15px]'
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div className='py-[40px]' >
+                        <div className='flex justify-center'>
+                            <Image
+                                src={'/New folder/USP_3.svg'}
+                                width={800}
+                                height={800}
+                                alt=''
+                                className='' />
+                        </div>
+                        <h2 className='text-[28px] font-bold leading-[34px] text-center mb-[5px] pt-[30px] text-[#220d4e]'>Do you have the tools to turn your insights into actions?</h2>
+                        <p className='text-[18px] leading-[30.34px] mb-[20px] text-center font-normal pt-[30px]  text-[#514b5f]'>Not all marketing activities are created equal. Our analytics suite simplifies complex data and gives you a unified view of campaign performance so you can make better, faster marketing choices at every stage of the customer journey.</p>
+                        <div className='flex items-center justify-center gap-x-[5px] hover:gap-x-[10px] cursor-pointer py-[10px] transition-all duration-300'>
+                            <div className='arrow'>
+                                <span className='text-[16px] font-medium leading-[21px] pb-[10px] btn-arrow text-[#220d4e]'>Explore our marketing analytics suite</span>
+                            </div>
+                            <div>
+                                <Image
+                                    src={"/right-arrow.svg"}
+                                    width={20}
+                                    height={20}
+                                    alt=''
+                                    className='w-[15px] h-[15px]'
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div className='py-[40px]'>
+                        <div className='flex justify-center'>
+                            <Image
+                                src={'/New folder/USP_4-1.svg'}
+                                width={800}
+                                height={800}
+                                alt=''
+                                className='' />
+                        </div>
+                        <h2 className='text-[28px] font-bold leading-[34px] text-center mb-[5px] text-[#220d4e] pt-[30px]'>Can you create exceptional experiences that keep customers engaged?</h2>
+                        <p className='text-[18px] leading-[30.34px] mb-[20px] text-center font-normal text-[#514b5f] pt-[40px]'>To succeed, you need to meet your customers where they are. Our customer experience and engagement suite, powered by a reliable deep linking engine, lets you create personalized journeys that increase conversions and return on experience.</p>
+                        <div className='flex items-center justify-center gap-x-[5px] hover:gap-x-[10px] cursor-pointer py-[10px] transition-all duration-300'>
+                            <div className='arrow'>
+                                <span className='text-[16px] font-medium leading-[21px] pb-[10px] btn-arrow text-[#220d4e]'>Explore our CX and deep linking suite</span>
+                            </div>
+                            <div>
+                                <Image
+                                    src={"/right-arrow.svg"}
+                                    width={20}
+                                    height={20}
+                                    alt=''
+                                    className='w-[15px] h-[15px]'
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div className='pt-[40px] pb-[50px]'>
+                        <div className='flex justify-center'>
+                            <Image
+                                src={'/New folder/USP_5-1.svg'}
+                                width={800}
+                                height={800}
+                                alt=''
+                                className='' />
+                        </div>
+                        <h2 className='text-[28px] font-bold leading-[34px] text-center mb-[5px] pt-[30px] text-[#220d4e]'>Is your budget safe from mobile ad fraud?</h2>
+                        <p className='text-[18px] leading-[30.34px] mb-[20px] text-center font-normal pt-[40px] text-[#514b5f]'>Bots and click farms aren’t going to generate revenue (for you). That’s why you need a comprehensive fraud protection solution to make sure you’re investing in the right channels, and only measuring and paying for real actions.</p>
+                        <div className='flex items-center justify-center gap-x-[5px] hover:gap-x-[10px] cursor-pointer py-[10px] transition-all duration-300'>
+                            <div className='arrow'>
+                                <span className='text-[16px] font-medium leading-[21px] pb-[10px] btn-arrow text-[#220d4e]'>Explore our fruad solutions</span>
+                            </div>
+                            <div>
+                                <Image
+                                    src={"/right-arrow.svg"}
+                                    width={20}
+                                    height={20}
+                                    alt=''
+                                    className='w-[15px] h-[15px]'
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MobileAppsflyer
